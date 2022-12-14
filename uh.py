670270c7e6256bb742c367f0bb67833095e906e1
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2022-12-14 19:45:40 +0100

    uh experiments

--- uh.py
+++ uh.py
@@ -1,14 +1,24 @@
 # %%
 import mainstream
+import umap.plot
 
 # %%
 u, e, c, l = mainstream.infer_uh_model(
-    n_neighbors=30, min_dist=0.01, min_cluster_size=1000, seeds=[None]
+    input="sparse_sw.npz",
+    output_prefix="sw",
+    # n_neighbors=30,
+    # min_dist=0.01,
+    min_cluster_size=1000,
+    seeds=[None],
 )
 
 # %%
 umap.plot.points(u, labels=l)
 
 # %%
-c.condensed_tree_.plot(selected_clusters=True)
-c.single_linkage_tree_.plot(selected_clusters=True)
+c.condensed_tree_.plot(select_clusters=True)
+
+# %%
+c.single_linkage_tree_.plot()
+
+# %%
