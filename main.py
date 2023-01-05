5fb78bf811a554924dfa95fb57a67f9f67dadd13
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2023-01-05 18:25:41 +0100

    overlap

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from mainstream import *
 min_year = 1946
 max_year = 2016
 suffix = "_stem_sw"
-# verbose = False
-# seeds = [1000, 1001, 1002, 1003, 1004]
+verbose = True
+seeds = [1000]
+overlap = False
 
 # %%
 print("json2sql")
@@ -25,36 +26,41 @@ print("clean raw")
 
 # %%
 print("clean data")
-clean_data(
-    input=f"clean{suffix}.db",
-    output=f"data{suffix}.db",
-    min_doc_occurencies=20,
-    max_doc_occurencies=10000,
-    min_word_freq=0,
-)
+# clean_data(
+#     input=f"clean{suffix}.db",
+#     output=f"data{suffix}.db",
+#     min_doc_occurencies=20,
+#     max_doc_occurencies=10000,
+#     min_word_freq=0,
+# )
 
 # %%
 print("create graph")
-# create_graph(input=f"data{suffix}.db", output=f"hsbm/graph{suffix}.gt.gz", overlap=False)
+osuffix = f"{suffix}_o" if overlap else suffix
+create_graph(
+    input=f"data{osuffix}.db", output=f"hsbm/graph{osuffix}.gt.gz", overlap=overlap
+)
 
 # %%
 print("create matrix")
-create_sparse(input=f"data{suffix}.db", output=f"sparse{suffix}.npz")
+# create_sparse(input=f"data{suffix}.db", output=f"sparse{suffix}.npz")
 
 # %%
 print("infer hsbm topic model")
-# infer_hsbm_tm(
-#    input=f"hsbm/graph{suffix}.gt.gz",
-#    output_prefix=f"hsbm/state{suffix}",
-#    verbose=verbose,
-#    seeds=seeds,
-# )
+osuffix = f"{suffix}_o" if overlap else suffix
+infer_hsbm_tm(
+    input=f"hsbm/graph{osuffix}.gt.gz",
+    output_prefix=f"hsbm/state{osuffix}",
+    verbose=verbose,
+    seeds=seeds,
+    overlap=overlap,
+)
 
 # %%
 print("dump hsbm topic model")
-# dump_hsbm_tm(
-#    graph_input=f"hsbm/graph{suffix}.gt.gz",
-#    input_prefix=f"hsbm/state{suffix}",
-#    output_prefix=f"hsbm/results{suffix}",
-#    seeds=seeds,
-# )
+dump_hsbm_tm(
+    graph_input=f"hsbm/graph{suffix}.gt.gz",
+    input_prefix=f"hsbm/state{suffix}",
+    output_prefix=f"hsbm/results{suffix}",
+    seeds=seeds,
+)
