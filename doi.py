df0a6be253dca6b6521dc5c3fdb64ebdcd8e5a49
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2023-01-26 18:13:27 +0100

    scopus metadata

--- doi.py
+++ doi.py
@@ -4,13 +4,13 @@ import time
 import re
 import urllib.parse
 
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from fake_useragent import UserAgent
+import dotenv
 
 import modin.pandas as pandas
 import requests
+import os
 
+dotenv.load_dotenv()
 
 # %%
 ### SemanticScholar
@@ -32,39 +32,32 @@ def get_doi(doi):
             return f"ERROR {r.status_code}"
 
 
-df = pandas.read_sql(
-    "SELECT id, authors, title, journal, year, doi FROM document", "sqlite:///data.db"
-)
-df["citations"] = df.apply(lambda row: get_doi(row.doi), axis=1)
-df.to_csv("citations.csv", index=False)
+# df = pandas.read_sql(
+#     "SELECT id, authors, title, journal, year, doi FROM document", "sqlite:///data.db"
+# )
+# df["citations"] = df.apply(lambda row: get_doi(row.doi), axis=1)
+# df.to_csv("citations.csv", index=False)
 # df[["id", "citations"]].set_index("id").to_sql("citations", "sqlite:///data.db")
 
 # %%
-### Google Scholar
-def get_jstor(url):
-    options = Options()
-    # options.headless = True
-    ua = UserAgent()
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument(f"user-agent={ua.random}")
-    options.add_argument("--profile-directory=Default")
-    driver = webdriver.Chrome(options=options)
-    driver.get(
-        f"https://scholar.google.com/scholar?hl=en&q={urllib.parse.quote(url, safe='')}"
-    )
-    html = driver.page_source
-    driver.quit()
-    if re.findall("recaptcha", html):
-        return "ERROR CAPTCHA"  # always a capthca
-    else:
-        cit = re.compile(r"Cited[\s\n]+by[\s\n]+([0-9]+)", re.MULTILINE).findall(html)
-        match len(cit):
-            case 0:
-                return "ERROR NOT FOUND"
-            case 1:
-                return cit[0]
-            case _:
-                return "ERROR MULTIPLE MATCHES"
-
-
+## SCOPUS
+key = os.environ["SCOPUS_API_KEY"]
+journals = [
+    "economic AND journal",
+    "journal AND political AND economy",
+    "review AND economic AND statistics",
+    "review AND economic AND studies",
+    "quarterly AND journal AND economics",
+    "economica",
+    "american AND economic AND review",
+    "econometrica",
+]
+responses = []
+for j in journals:
+    query = urllib.parse.quote_plus(f"SRCTITLE({j})", safe="")
+    url = f"https://api.elsevier.com/content/search/scopus?apiKey={key}&query={query}"
+    r = requests.get(url)
+    print(r.status_code)
+    responses += r.json()["search-results"]["entry"]
+pandas.DataFrame(responses).to_csv("scopus.csv", index=False)
 # %%
