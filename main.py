59ca2ac421f020179ac32fc0cc79fe054d29bc2e
Michele 'TnTo' Ciruzzi <tnto@hotmail.it>
2023-02-07 20:53:32 +0100

    uncomment final version

--- main.py
+++ main.py
@@ -5,57 +5,56 @@ from mainstream import *
 suffix = ""
 verbose = True
 seeds = [1000, 1001, 1002]
-overlap = True
 
 # %%
 print("json2sql")
-# json2sql()
+json2sql()
 
 # %%
 print("clean raw")
-# clean_raw(
-#     output=f"clean{suffix}.db",
-#     tmp=f"merge{suffix}.db",
-#     min_word_len=3,
-#     clean_fn=clean_and_stem,
-# )
+clean_raw(
+    output=f"clean{suffix}.db",
+    tmp=f"merge{suffix}.db",
+    min_word_len=3,
+    clean_fn=clean_and_stem,
+)
 
 # %%
 print("clean data")
-# clean_data(
-#     input=f"clean{suffix}.db",
-#     output=f"data{suffix}.db",
-#     min_doc_len=1500,  # ~3/4 pp
-#     min_doc_occurencies=35,  # 1/1.000 of docs
-#     max_doc_occurencies=9000,  # ~25% of docs
-#     min_word_freq=0,
-#     min_word_len=0,
-#     sw="latex.csv",
-# )
+clean_data(
+    input=f"clean{suffix}.db",
+    output=f"data{suffix}.db",
+    min_doc_len=1500,  # ~3/4 pp
+    min_doc_occurencies=35,  # 1/1.000 of docs
+    max_doc_occurencies=9000,  # ~25% of docs
+    min_word_freq=0,
+    min_word_len=0,
+    sw="latex.csv",
+)
 
 # %%
 print("create graph")
-# create_graph(input=f"data{suffix}.db", output=f"graph{osuffix}.gt.gz")
+create_graph(input=f"data{suffix}.db", output=f"graph{suffix}.gt.gz")
 
 # %%
 print("create matrix")
-# create_sparse(input=f"data{suffix}.db", output=f"sparse{suffix}.npz")
+create_sparse(input=f"data{suffix}.db", output=f"sparse{suffix}.npz")
 
 # %%
 print("infer hsbm topic model")
-# infer_hsbm_tm(
-#     input=f"graph{suffix}.gt.gz",
-#     output_prefix=f"state{suffix}",
-#     verbose=verbose,
-#     seeds=seeds,
-# )
+infer_hsbm_tm(
+    input=f"graph{suffix}.gt.gz",
+    output_prefix=f"state{suffix}",
+    verbose=verbose,
+    seeds=seeds,
+)
 
 # %%
 print("dump hsbm topic model")
-# dump_hsbm_tm(
-#     graph_input=f"graph{suffix}.gt.gz",
-#     input_prefix=f"state{suffix}",
-#     model_output=f"model{suffix}.db",
-#     entropy_output=f"entropy{suffix}.csv",
-#     seeds=seeds,
-# )s
+dump_hsbm_tm(
+    graph_input=f"graph{suffix}.gt.gz",
+    input_prefix=f"state{suffix}",
+    model_output=f"model{suffix}.db",
+    entropy_output=f"entropy{suffix}.csv",
+    seeds=seeds,
+)
